c945925b1260bbc81594dd4f63d4a54c4c3a4612
Chandra kishor Gauro <ckgauro@gmail.com>
2020-11-30 18:51:09 -0500

    Init FIles

---
+++ src/main/java/com/gauro/springbootoracleexample/SpringBootOracleExampleApplication.java
@@ -0,0 +1,13 @@
+package com.gauro.springbootoracleexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOracleExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootOracleExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gauro/springbootoracleexample/SpringBootOracleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gauro.springbootoracleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOracleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
